f10846c5d609739c9add38066aae74e73840e656
Sensationz Media and Arts Pvt. Ltd. <sensationzdigital@gmail.com>
2025-03-03 17:24:37 +0530

    Done

--- src/app/(ADMIN)/admin/page.tsx
+++ src/app/(ADMIN)/admin/page.tsx
@@ -11,7 +11,7 @@ const page = async () => {
     <div className="mx-8">
       <h1 className="text-3xl font-bold">Hello, {session?.user.name} </h1>
       <StatsCard />
-      <div className="mb-96 grid w-full grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="mb-40 grid w-full grid-cols-1 gap-4 md:grid-cols-2">
         <BarCharts dep={dep} />
         <div className="">
           <RightSide />
